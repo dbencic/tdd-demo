d3652f346eac3aae0b43571106f6e75c49fd8b9c
dbencic <dragan.bencic@infobip.com>
2020-04-03 18:54:59 +0200

    first test

--- src/test/example.spec.js
+++ src/test/example.spec.js
@@ -1,6 +1,9 @@
 var assert = require('assert');
+const fizzBuzz = require("../fizzBuzz");
 describe('fizz buz problem', function () {
- it('fizzBuyy should be a function', function () {
-        assert.fail();
+    it('fizzBuzz should be a function', function () {
+        fizzBuzz();
     });
 });
+
+//R-G-R
