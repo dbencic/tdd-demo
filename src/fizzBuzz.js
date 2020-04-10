8a07f592c32daa55e6e05b6e59ad19f2d5658b32
dbencic <dragan.bencic@infobip.com>
2020-04-10 18:06:05 +0200

    Dovršen FizzBuzz

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,9 @@
-function fizzBuzz() {
-
+//R-G-R
+function fizzBuzz(num) {
+    let result = [];
+    if(num%3 == 0) result.push("Fizz");
+    if(num%5 == 0) result.push("Buzz");
+    return result.join("") || undefined;
 }
 
 module.exports = fizzBuzz;
